37fa61d5ed36266fe60f0a2eaaf71fce509c98af
filzarr <filzaramadhan2003@students.usu.ac.id>
2024-02-09 04:08:17 +0700

    perbaikan title page2

--- resources/views/layout/form.blade.php
+++ resources/views/layout/form.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>  Form DPR RI DAPIL SUMUT 3  |  Form</title>
+    <title>  Form DPR RI DAPIL SUMUT 3 </title>
     <link rel="stylesheet" href="{{url('css/_form-class.css')}}">
     @stack('css')@vite(['resources/css/app.css','resources/js/app.js'])
     @livewireStyles
