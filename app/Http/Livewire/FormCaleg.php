40b049a95b927de16e0fbca909035f88cff746bb
filzarr <filzaramadhan2003@students.usu.ac.id>
2024-02-09 03:08:17 +0700

    penambahan loading submit

--- app/Http/Livewire/FormCaleg.php
+++ app/Http/Livewire/FormCaleg.php
@@ -37,7 +37,7 @@ class FormCaleg extends Component
     public $suaratidaksah = 0;
     public $totalsuarasahtidak;
     public $pemilih = 0;
-    public $isLoading = false;
+    public $show = false;
     protected $rules = [
         'lampiran1' => 'required|max:5000000',
         'partai' => 'required|integer',
@@ -121,7 +121,7 @@ class FormCaleg extends Component
     }
     public function submit(){
         $this->validate(); 
-        $this->isLoading = true;
+        $this->show = true;
         DataPemilih::create([
             'tps_id' => $this->tps,
             'kategori' => 'dpt',
@@ -172,7 +172,7 @@ class FormCaleg extends Component
                 'file' => $img,
             ]);
         }
-        $this->isLoading = false;
+        $this->show = false;
         Alert::success('Berhasil Mengisi Data', 'Terima Kasih Telah Melakukan Pengisian Data Suara');
         return redirect('/form');
        
